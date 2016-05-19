7848a95795efe1385200b33b0d22343a55ca13a9
oanylund <ole.a.nylund@gmail.com>
2016-05-19 18:13:13 +0200

    Added count to result on get users endpoint. Also added filter on firstname and lastname

--- routes/user.js
+++ routes/user.js
@@ -7,23 +7,41 @@ var User = models.User;
 function requestFilter(query) {
 
   var filter = {}
-  var orderDirection = query.order || 'ASC'
-  var orderBy = query.orderBy || 'id'
-  var orderFirst = []
-  var order = []
-  orderFirst.push(orderBy,orderDirection)
-  order.push(orderFirst)
-  filter.order = order
+  var orderDirection = query.order || 'ASC';
+  var orderBy = query.orderBy || 'id';
+  var orderFirst = [];
+  var order = [];
+  orderFirst.push(orderBy,orderDirection);
+  order.push(orderFirst);
+  filter.order = order;
   if (query.limit) { filter.limit = +query.limit }
   if (query.offset) { filter.offset = +query.offset }
 
-  return filter
+  var where = {}
+  if (query.firstName) {
+    where = Object.assign(where, {
+      firstName: {
+        $like: '%' + query.firstName + '%'
+      }
+    });
+  }
+  if (query.lastName) {
+    where = Object.assign(where, {
+      lastName: {
+        $like: '%' + query.lastName + '%'
+      }
+    });
+  }
+
+  filter.where = where;
+
+  return filter;
 }
 
 userRoute.get('/', function(req,res,next) {
 
-  User.findAll(requestFilter(req.query))
-  .then(function(rows) {
+  User.findAndCountAll(requestFilter(req.query))
+  .then( function(rows) {
     res.json({ success: true, data: rows });
   })
   .catch( function(err) {
